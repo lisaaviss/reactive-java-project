146a7b1ab8599acb2a51c34fc69e413a9f675eef
Artem Lisunov <lisaaviss@niuitmo.ru>
2025-01-12 00:10:32 +0300

    Initial commit

---
+++ src/main/java/com/example/reactivejavaproject/ReactiveJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactivejavaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveJavaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveJavaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reactivejavaproject/ReactiveJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivejavaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveJavaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
